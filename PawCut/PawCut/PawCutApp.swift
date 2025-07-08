c03c80d849cd3042c562523c0692be9ca87c5669
YeongJin Jeong <yungzin98@naver.com>
2025-07-08 19:53:51 +0900

    chore: initial commit

---
+++ PawCut/PawCut/PawCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawCutApp.swift
+//  PawCut
+//
+//  Created by Luminouxx on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PawCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
