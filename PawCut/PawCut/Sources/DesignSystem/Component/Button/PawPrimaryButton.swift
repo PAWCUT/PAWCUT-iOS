a3d14260fc8dfa05ca688957607acb98f6546d09
YeongJin Jeong <yungzin98@naver.com>
2025-07-15 00:16:34 +0900

    feat: PawPrimaryButton 상하좌우 패딩 조절할 수 있도록 변경

--- PawCut/PawCut/Sources/DesignSystem/Component/Button/PawPrimaryButton.swift
+++ PawCut/PawCut/Sources/DesignSystem/Component/Button/PawPrimaryButton.swift
@@ -11,28 +11,43 @@ struct PawPrimaryButton: View {
     
     @State private var isEnabled: Bool = true
     
-    let title: String
-    let action: () -> Void
+    private let title: String
+    private let action: () -> Void
+    private let horizontalPadding: CGFloat
+    private let verticalPadding: CGFloat
+    
+    init(
+        _ title: String,
+        horizontalPadding: CGFloat = 20,
+        verticalPadding: CGFloat = 20,
+        action: @escaping () -> Void
+    ) {
+        self.title = title
+        self.action = action
+        self.horizontalPadding = horizontalPadding
+        self.verticalPadding = verticalPadding
+    }
     
     var body: some View {
         Button(action: action) {
             Text(title)
                 .pretendardFont(size: ._16, weight: .semibold)
                 .padding(EdgeInsets(top: 20, leading: 0, bottom: 20, trailing: 0))
-                .foregroundColor(Color(.grayScale06))
+                .foregroundColor(.grayScale06)
                 .frame(maxWidth: .infinity)
                 .background(
                     RoundedRectangle(cornerRadius: 12)
                         .fill(isEnabled ? .pointPurple01 : .grayScale03)
                 )
         }
-        .padding(20)
+        .padding(.horizontal, horizontalPadding)
+        .padding(.vertical, verticalPadding)
         .buttonStyle(PlainButtonStyle())
     }
 }
 
 #Preview {
-    PawPrimaryButton(title: "안녕하세요") {
-        print("hello")
+    PawPrimaryButton("PrimaryButton") {
+        
     }
 }
