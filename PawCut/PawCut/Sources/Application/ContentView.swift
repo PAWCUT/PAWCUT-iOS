e361a67dd737586201fc882b9efd8802f9fc68c7
YeongJin Jeong <yungzin98@naver.com>
2025-08-11 16:02:41 +0900

    feat: ContentView에 NavigationStack 추가

--- PawCut/PawCut/Sources/Application/ContentView.swift
+++ PawCut/PawCut/Sources/Application/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var navigationManager = NavigationManager.shared
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $navigationManager.path) {
+            navigationManager.getRootView()
+                .navigationDestination(for: AppDestination.self) { destination in
+                    destination.view()
+                }
         }
-        .padding()
     }
 }
 
