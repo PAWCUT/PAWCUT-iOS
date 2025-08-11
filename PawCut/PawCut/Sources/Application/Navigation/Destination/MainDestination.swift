4979845aded19164298a392d414832251996f546
YeongJin Jeong <yungzin98@naver.com>
2025-08-11 16:01:53 +0900

    feat: Navigation Destination 추가
    + Onboarding
    + Main
    
    2개의 플로우로 분기

---
+++ PawCut/PawCut/Sources/Application/Navigation/Destination/MainDestination.swift
@@ -0,0 +1,21 @@
+//
+//  MainDestination.swift
+//  PawCut
+//
+//  Created by Luminouxx on 8/11/25.
+//
+
+import SwiftUI
+
+enum MainDestination: NavigationDestination {
+    case home
+    
+    @ViewBuilder
+    func view() -> some View {
+        switch self {
+        case .home:
+            // TODO: HomeView로 수정 예정
+            EmptyView()
+        }
+    }
+}
---
+++ PawCut/PawCut/Sources/Application/Navigation/Destination/NavigationDestination.swift
@@ -0,0 +1,30 @@
+//
+//  NavigationDestination.swift
+//  PawCut
+//
+//  Created by Luminouxx on 8/11/25.
+//
+
+import SwiftUI
+
+protocol NavigationDestination: Hashable, CaseIterable {
+    associatedtype ViewType: View
+    @ViewBuilder func view() -> ViewType
+}
+
+enum AppDestination: Hashable {
+    case onboarding(OnboardingDestination)
+    case main(MainDestination)
+}
+
+extension AppDestination {
+    @ViewBuilder
+    func view() -> some View {
+        switch self {
+        case .onboarding(let destination):
+            destination.view()
+        case .main(let destination):
+            destination.view()
+        }
+    }
+}
