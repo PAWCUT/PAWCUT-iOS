f5273e902e47b110992d7d21f53a3118d74ecf77
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-08-12 21:13:03 +0900

    fix: 비어있음 강아지 고양이 이미지 이름 바꿈

---
+++ PawCut/PawCut/Sources/Views/Archive/Components/EmptyStateView.swift
@@ -0,0 +1,18 @@
+//
+//  emptyStateView.swift
+//  PawCut
+//
+//  Created by taeni on 8/12/25.
+//
+
+import SwiftUI
+
+struct emptyStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    emptyStateView()
+}
