a4a1c82113e678364b530cd59b8b7679c0c2a12f
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-08-14 02:12:00 +0900

    feat: 앱샌드박스에서 UIImage 가져올 수 있도록 처리
    논의할 것들이 많음
    1. 로딩 중 이미지 필요?
    2. 이미지에 문제가 있을 시 어떻게 보여줄 것인지?

---
+++ PawCut/PawCut/Sources/DesignSystem/Component/Image/AsyncPhotoImageView.swift
@@ -0,0 +1,41 @@
+//
+//  AsyncPhotoImageView.swift
+//  PawCut
+//
+//  Created by taeni on 8/13/25.
+//
+
+import SwiftUI
+
+struct AsyncPhotoImageView: View {
+    
+    // UIImage 를 가져옴
+    let fileName: String
+    
+    @State private var image: UIImage?
+    @State private var isLoading = true
+    
+    var body: some View {
+        Group {
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } else if isLoading {
+                // TODO: Loading 이미지 필요
+                ImageComponent(imageName: "mock_sample", size: CGSize(width: 290, height: 440))
+                //                ProgressView()
+                //                    .scaleEffect(0.8)
+                //                    .foregroundColor(.grayScale03)
+            } else {
+                ImageComponent(imageName: "mock_sample", size: CGSize(width: 290, height: 440))
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.grayScale05)
+    }
+}
+
+#Preview {
+    AsyncPhotoImageView(fileName: "mock_template")
+}
