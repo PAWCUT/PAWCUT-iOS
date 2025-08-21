731d961568037285117f611c6530e7f290a24a19
YeongJin Jeong <yungzin98@naver.com>
2025-08-21 09:33:38 +0900

    fix: FetInfoVIew UI수정

---
+++ PawCut/PawCut/Sources/ViewModels/Setting/FixPetInfoViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  FixPetInfoViewModel.swift
+//  PawCut
+//
+//  Created by Luminouxx on 8/20/25.
+//
+
+import Foundation
+
+@MainActor
+class FixPetInfoViewModel: ObservableObject {
+    private let navigationManager = NavigationManager.shared
+    
+    @Published var name: String = ""
+    @Published var selectedType: PetType? = nil
+    @Published var isDogEnabled = false
+    @Published var isCatEnabled = false
+    @Published var isError: Bool = false
+    
+    var isValidInput: Bool {
+        !name.isEmpty && name.count <= 5 && selectedType != nil
+    }
+
+    func updateName(_ newName: String) {
+        name = newName
+        validateName()
+    }
+
+    func selectDog() {
+        isDogEnabled = true
+        isCatEnabled = false
+        selectedType = .dog
+    }
+
+    func selectCat() {
+        isDogEnabled = false
+        isCatEnabled = true
+        selectedType = .cat
+    }
+
+    func tapStartButton() {
+        guard isValidInput else { return }
+        navigationManager.startMainFlow()
+    }
+
+    private func validateName() {
+        if name.count > 5 || name.isEmpty {
+            isError = true
+        } else {
+            isError = false
+        }
+    }
+}
--- PawCut/PawCut/Sources/Views/Setting/FixPetInfoView.swift
+++ PawCut/PawCut/Sources/Views/Setting/FixPetInfoView.swift
@@ -10,86 +10,72 @@
 import SwiftUI
 
 struct FixPetInfoView: View {
-    @StateObject private var viewModel = PetInfoViewModel()
-    @Environment(\.dismiss) private var dismiss
+    @StateObject private var viewModel = FixPetInfoViewModel()
     
     var body: some View {
-        NavigationView {
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        PawTitleLabel
-                            .bold24(
-                                "반려동물의\n정보를 입력해주세요.",
-                                alignment: .leading,
-                                lineLimit: 2
-                            )
-                            .frame(height: 70,alignment: .topLeading)
-                            .padding(.bottom, 44)
-                            .padding(.top, 20)
-                        PawTitleLabel
-                            .semi14(
-                                "이름",
-                                color: .grayScale02,
-                                alignment: .leading,
-                                lineLimit: 2
-                            )
-                        //Text 입력칸
-                        PawTextField(
-                            "이름을 입력해 주세요.",
-                            text: $viewModel.name,
-                            isError: $viewModel.isError,
-                            errorMessage: "공백없이 1자 이상 5자 이하로 입력해주세요."
-                        )
-                        .padding(.bottom, 28)
-                        .onChange(of: viewModel.name) { oldValue, newValue in
-                            viewModel.updateName(newValue)
-                        }
-                        
-                        PawTitleLabel
-                            .semi14(
-                                "종류",
-                                color: .grayScale02,
-                                alignment: .leading,
-                                lineLimit: 2
-                            )
-                        HStack(spacing: 12) {
-                            PawChoiceButton(
-                                "강아지",
-                                isEnabled: viewModel.isDogEnabled,
-                                horizontalPadding: 0
-                            ) {
-                                viewModel.selectDog()
-                            }
-                            PawChoiceButton(
-                                "고양이",
-                                isEnabled: viewModel.isCatEnabled,
-                                horizontalPadding: 0
-                            ) {
-                                viewModel.selectCat()
-                            }
-                        }
-                        Spacer()
-                    }
-                }
-                .padding(.horizontal, 20)
+        VStack {
+            VStack(alignment: .leading) {
+                PawTitleLabel
+                    .bold24(
+                        "반려동물의\n정보를 입력해주세요.",
+                        alignment: .leading,
+                        lineLimit: 2
+                    )
+                    .frame(height: 70,alignment: .topLeading)
+                    .padding(.bottom, 44)
+                    .padding(.top, 36)
                 
-                PawPrimaryButton("시작하기", isEnabled: viewModel.isValidInput) {
-                    // TODO: 화면 연결
+                PawTitleLabel
+                    .semi14(
+                        "이름",
+                        color: .grayScale02,
+                        alignment: .leading,
+                        lineLimit: 2
+                    )
+                
+                PawTextField(
+                    "이름을 입력해 주세요.",
+                    text: $viewModel.name,
+                    isError: $viewModel.isError,
+                    errorMessage: "공백없이 1자 이상 5자 이하로 입력해주세요."
+                )
+                .padding(.bottom, 28)
+                .onChange(of: viewModel.name) { oldValue, newValue in
+                    viewModel.updateName(newValue)
                 }
-            }
-            .navigationTitle("프로필")
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarBackButtonHidden(true)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        dismiss()
-                    }) {
-                        Image(systemName: "chevron.left")
-                            .foregroundColor(.grayScale01)
+                
+                PawTitleLabel
+                    .semi14(
+                        "종류",
+                        color: .grayScale02,
+                        alignment: .leading,
+                        lineLimit: 2
+                    )
+                HStack(spacing: 12) {
+                    PawChoiceButton(
+                        "강아지",
+                        isEnabled: viewModel.isDogEnabled,
+                        horizontalPadding: 0
+                    ) {
+                        viewModel.selectDog()
+                    }
+                    
+                    PawChoiceButton(
+                        "고양이",
+                        isEnabled: viewModel.isCatEnabled,
+                        horizontalPadding: 0
+                    ) {
+                        viewModel.selectCat()
                     }
                 }
+                Spacer()
+            }
+            .padding(20)
+            
+            Spacer()
+            
+            PawPrimaryButton("저장하기", isEnabled: viewModel.isValidInput) {
+                viewModel.tapStartButton()
             }
         }
     }
