863c7d0c09653a869094af653b535bb7a2bc8c6d
YeongJin Jeong <yungzin98@naver.com>
2025-08-25 16:37:38 +0900

    feat: Splash View 추가

--- PawCut/PawCut/Sources/Application/PawCutApp.swift
+++ PawCut/PawCut/Sources/Application/PawCutApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct PawCutApp: App {
+    @State private var showSplash = true
     
     init() {
         FontSet.registerFonts()
@@ -16,7 +17,34 @@ struct PawCutApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ZStack {
+                ContentView()
+                    .scaleEffect(showSplash ? 0.95 : 1.0)
+                    .opacity(showSplash ? 0 : 1)
+                
+                if showSplash {
+                    SplashView()
+                        .transition(.asymmetric(
+                            insertion: .identity,
+                            removal: .opacity.combined(with: .scale(scale: 1.1))
+                        ))
+                        .zIndex(1)
+                }
+            }
+            .onAppear {
+                hideSplash()
+            }
+            .animation(.easeInOut(duration: 0.8), value: showSplash)
+        }
+    }
+}
+
+extension PawCutApp {
+    func hideSplash() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(.easeInOut(duration: 0.8)) {
+                showSplash = false
+            }
         }
     }
 }
