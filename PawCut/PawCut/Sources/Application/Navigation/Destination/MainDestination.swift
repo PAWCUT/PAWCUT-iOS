5d6fd365a9bbde09b49f2e1865059ba88edf7b2e
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-08-25 21:32:46 +0900

    fix: 이미 작업되어있는 inquiry, terms 파일 원상복구
    제 기존 파일을 덮어씌워서 다시 복구시켰습니다.

--- PawCut/PawCut/Sources/Application/Navigation/Destination/MainDestination.swift
+++ PawCut/PawCut/Sources/Application/Navigation/Destination/MainDestination.swift
@@ -16,8 +16,6 @@ enum MainDestination: NavigationDestination {
     case setting
     case soundSetting
     case fixPetInfo
-    case inquiry
-    case terms
     case petInfo
     case tip
     case ready
@@ -40,10 +38,6 @@ enum MainDestination: NavigationDestination {
             SoundSettingView()
         case .fixPetInfo:
             FixPetInfoView()
-        case .inquiry:
-            EmptyView() // TODO: InquiryView 구현 예정
-        case .terms:
-            EmptyView() // TODO: TermsView 구현 예정
         case .petInfo:
             PetInfoView()
         case .pawcutSelection:
