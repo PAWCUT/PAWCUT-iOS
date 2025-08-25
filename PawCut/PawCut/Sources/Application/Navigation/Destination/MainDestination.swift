987563482f638ee5209aee2ca0470510561c3375
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-08-25 21:18:52 +0900

    fix: NavigationDestination 에서 PhotoDetailsView enum 추가
    MainDestination 에서 photoDetails Enum을 추가하고,
    NavigationDestination 에서 CaseIterable 을 제거했습니다.

--- PawCut/PawCut/Sources/Application/Navigation/Destination/MainDestination.swift
+++ PawCut/PawCut/Sources/Application/Navigation/Destination/MainDestination.swift
@@ -10,11 +10,14 @@ import SwiftUI
 enum MainDestination: NavigationDestination {
     case home
     case archive
+    case photoDetails(date: Date, index: Int)
     case pawcutFrameSelection
     case pawcutSelection
     case setting
     case soundSetting
     case fixPetInfo
+    case inquiry
+    case terms
     case petInfo
     case tip
     case ready
@@ -27,6 +30,8 @@ enum MainDestination: NavigationDestination {
             MainView()
         case .archive:
             ArchiveView()
+        case .photoDetails(let date, let index):
+            PhotoDetailsView(initialDate: date, initialIndex: index)
         case .pawcutFrameSelection:
             PawcutFrameView()
         case .setting:
@@ -35,6 +40,10 @@ enum MainDestination: NavigationDestination {
             SoundSettingView()
         case .fixPetInfo:
             FixPetInfoView()
+        case .inquiry:
+            EmptyView() // TODO: InquiryView 구현 예정
+        case .terms:
+            EmptyView() // TODO: TermsView 구현 예정
         case .petInfo:
             PetInfoView()
         case .pawcutSelection:
