b966f87b1f383515562aee46426deb18312bd51d
허찬욱 <101908750+hkwangro@users.noreply.github.com>
2025-09-21 17:19:41 +0900

    refactor: PawcutReady
    - Subview 추가
    - PawLabel 컴포넌트로 변경

--- PawCut/PawCut/Sources/Views/PawcutCamera/PawcutReadyView.swift
+++ PawCut/PawCut/Sources/Views/PawcutCamera/PawcutReadyView.swift
@@ -15,25 +15,10 @@ struct PawcutReadyView: View {
         ZStack {
             Color.grayScale06
                 .ignoresSafeArea()
-
-            // TODO: 스택을 쓸 경우, 생각을 많이 해보자, 컴포넌트 SubView로 분리
+            
             VStack(spacing: 0) {
                 Spacer()
-
-                // TODO: PawLabel 컴포넌트로 변경
-                VStack(spacing: 28) {
-                    Image("onboarding_1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 190, height: 217)
-                    
-                    // TODO: PawLabel 컴포넌트로 변경
-                    Text("촬영이 바로 시작됩니다\n준비해 주세요!")
-                        .pretendardFont(size: ._20, weight: .semibold)
-                        .foregroundColor(.grayScale01)
-                        .multilineTextAlignment(.center)
-                }
-
+                PawcutReadySubView()
                 Spacer()
             }
         }
