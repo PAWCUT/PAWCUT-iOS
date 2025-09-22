0613f38097b2d991d693545f9bc3f4bd8275f8ec
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-09-22 21:38:34 +0900

    refactor: archiveView 에 이미지 없을 시 EmptyView 화면 리팩토링

--- PawCut/PawCut/Sources/Views/Archive/SubViews/Empty/ArchiveEmptyView.swift
+++ PawCut/PawCut/Sources/Views/Archive/SubViews/Empty/ArchiveEmptyView.swift
@@ -20,16 +20,10 @@ struct ArchiveEmptyView: View {
                 size: CGSize(width: 100, height: 100)
             )
             
-            VStack(spacing: 12) {
-                PawTitleLabel.semi18("비어있음", color: .grayScale01)
-                
-                PawBodyLabel.med14(
-                    "아직 찍은 사진이 없습니다.\n오늘의 하루를 남겨보세요.",
-                    color: .grayScale03,
-                    alignment: .center,
-                    lineLimit: 2
-                )
-            }
+            EmptyContent(
+                stateString: "비어있음",
+                explainString: "아직 찍은 사진이 없습니다.\n오늘의 하루를 남겨보세요."
+            )
             
             PawTakeCutButton("포우컷 촬영하러 가기") {
                 didTapTakeCutButton()
---
+++ PawCut/PawCut/Sources/Views/Archive/SubViews/Empty/EmptyContent.swift
@@ -0,0 +1,30 @@
+//
+//  EmptyContent.swift
+//  PawCut
+//
+//  Created by taeni on 9/22/25.
+//
+
+import SwiftUI
+
+struct EmptyContent: View {
+    let stateString: String
+    let explainString: String
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            PawTitleLabel.semi18(stateString, color: .grayScale01)
+            
+            PawBodyLabel.med14(
+                explainString,
+                color: .grayScale03,
+                alignment: .center,
+                lineLimit: 2
+            )
+        }
+    }
+}
+
+#Preview {
+    EmptyContent(stateString: "비어있음", explainString: "아직 찍은 사진이 없습니다.\n오늘의 하루를 남겨보세요.")
+}
